a812a1515477bd5ec478b650b1f401a6df352ef5
slashpage15github <107419267+slashpage15github@users.noreply.github.com>
2023-11-14 18:40:07 -0600

    registro

--- App.py
+++ App.py
@@ -1,8 +1,47 @@
 from flask import Flask, render_template, request, url_for, flash, redirect
+import model.package_model.aspirantes as aspirantes
 import model.package_model.Empresa as Empresa
 import model.package_model.Curso as Curso
 
+import model.package_model.Empresa as Empresa
+import model.package_model.Curso as Curso
+
+import model.package_model.AspirantesCursos as AspirantesCursos
+
+#import metodos static
+from model.package_model.aspirantes import Aspirantes
+from model.package_model.AspirantesCursos import AspirantesCursos
+from datetime import datetime,date,time
+#import metodos static
+#from model.package_model.Curso import Curso
+import jsonpickle
+import json
+
 app = Flask(__name__)
+
+def datetime_handler(obj):
+    if isinstance(obj, (datetime, date, time)):
+        return str(obj)
+
+@app.route("/valida_existe", methods=['GET','POST'])
+def valida_aspirante():
+    rfc=request.form['f_rfc'].upper()
+    cuantos_aspiran=Aspirantes.existe_aspirante(rfc)
+    return jsonpickle.encode(cuantos_aspiran)
+
+@app.route("/get_aspirante", methods=['GET','POST'])
+def get_aspirante():
+    rfc=request.form['f_rfc'].upper()
+    dato_aspirante=Aspirantes.obtener_aspirante_por_rfc(rfc)
+    return json.dumps(dato_aspirante,default=datetime_handler)
+
+@app.route("/valida_existe_rfc_curso", methods=['GET','POST'])
+def valida_aspirante_rfc_curso():
+    rfc=request.form['f_rfc'].upper()
+    curso=request.form['f_id_curso']
+    cuantos_aspirancursos=AspirantesCursos.existe_aspirantecursos(curso,rfc)
+    return jsonpickle.encode(cuantos_aspirancursos)
+
 
 @app.route("/")
 @app.route("/index")
@@ -17,6 +56,44 @@ def add_aspirante():
         lista_cursos = obj_cur.obtener_cursos()
         return render_template('aspirante.html',lista_empresas=lista_empresas,lista_cursos=lista_cursos)
 
+@app.route("/registra",methods=['POST'])
+def registra_aspirante():
+    _rfc=request.form['f_rfc'].upper()
+    _cur=request.form['f_id_curso']
+    _fec=datetime.now()
+    
+    cuantos_aspiran=Aspirantes.existe_aspirante(_rfc)
+    cuantos_aspirancursos=AspirantesCursos.existe_aspirantecursos(_cur,_rfc)
+    if cuantos_aspiran==1:
+        if cuantos_aspirancursos==1:
+            flash(f"Aspirante y curso ya registrado","Error")
+            return redirect(url_for('add_aspirante'))
+        else:
+            obj_aspcur=AspirantesCursos(_cur,_rfc,_fec)
+            res_aspcur=obj_aspcur.insertar_aspirantecursos(obj_aspcur)
+            flash(f"Curso agregado correctamente","success")
+            return redirect(url_for('add_aspirante'))
+    else:
+        _nom=request.form['f_nombre'].upper()    
+        _pat=request.form['f_paterno'].upper()    
+        _mat=request.form['f_materno'].upper()    
+        _emp=request.form['f_id_empresa']    
+        _tel=request.form['f_telefono']    
+        _ema=request.form['f_email']    
+        _cur=request.form['f_id_curso']
+        _fec=datetime.now()
+        obj_asp= aspirantes.Aspirantes(_rfc,_nom,_pat,_mat,_emp,_tel,_ema,_fec)
+        res=obj_asp.insertar_aspirante(obj_asp)
+        
+        obj_aspcur=AspirantesCursos(_cur,_rfc,_fec)
+        res_aspcur=obj_aspcur.insertar_aspirantecursos(obj_aspcur)
+
+        #return _rfc+'-'+_nom+'-'+_pat+'-'+_mat+'-'+_emp+'-'+_tel+'-'+_ema+'-'+_cur
+        flash(f"Aspirante y curso registrado correctamente","success")
+        return redirect(url_for('add_aspirante'))  
+        #return render_template('aspirante.html')
+        #return res
+        
 @app.route("/lista_cursos")
 def lista_cursos():
     return "lista de cursos"
---
+++ model/package_model/AspirantesCursos.py
@@ -0,0 +1,85 @@
+import model.package_model.Database as Database
+from flask import jsonify
+class AspirantesCursos:
+    def __init__(self, id_curso, rfc, fecha_registro):
+        self.__id_curso=id_curso
+        self.__rfc=rfc
+        self.__fecha_registro=fecha_registro
+    
+    @staticmethod    
+    def existe_aspirantecursos(id_curso,rfc):
+        conexion = conexion = Database.Database()
+        aspirantecurso = None
+        with conexion.cursor as cursor:
+            cursor.execute(
+                "SELECT count(*) as ex FROM aspirantes_cursos WHERE ID_CURSO = %s and RFC= %s",(id_curso , rfc))
+            aspirantecurso = cursor.fetchone()
+        conexion.conn.close()
+        #return jsonify(aspirante[0])    
+        return aspirantecurso[0]    
+
+    @staticmethod    
+    def existe_curso(id_curso):
+        conexion = conexion = Database.Database()
+        aspirantecurso = None
+        with conexion.cursor as cursor:
+            cursor.execute(
+                "SELECT count(*) as ex FROM aspirantes_cursos WHERE ID_CURSO = %s",(id_curso))
+            aspirantecurso = cursor.fetchone()
+        conexion.conn.close()
+        #return jsonify(aspirante[0])    
+        return aspirantecurso[0]
+    
+    def insertar_aspirantecursos(self, obj_asp):
+        #return obj_asp.__rfc
+        #return obj_asp
+        conexion = Database.Database()
+        with conexion.cursor as cursor:
+            try:
+                query="INSERT INTO aspirantes_cursos(ID_CURSO,RFC,FECHA_REGISTRO) VALUES (%s, %s, %s)"
+                vals=(obj_asp.__id_curso,obj_asp.__rfc,obj_asp.__fecha_registro)
+                #return (query % vals) ver sentencia
+                affected=cursor.execute(query,vals)
+                conexion.conn.commit()
+                return str(cursor.rowcount)
+            except Exception as e:
+                return e
+            except pymysql.err.ProgrammingError as except_detail:
+                return print("pymysql.err.ProgrammingError: «{}»".format(except_detail))
+            finally:
+                conexion.conn.close() 
+    
+    def eliminar_aspirantecursos(self,id_curso,rfc):
+        conexion = Database.Database()
+        with conexion.cursor as cursor:
+            affected=cursor.execute("DELETE FROM aspirantes_cursos WHERE ID_CURSO = %s and RFC=%s", id_curso,rfc)
+        conexion.conn.commit()
+        conexion.conn.close()
+        return affected
+    
+    def actualizar_aspirantecursos(self,id_curso,rfc):
+        conexion = Database.Database()
+        with conexion.cursor as cursor:
+            affected=cursor.execute("UPDATE aspirantes_cursos SET ID_CURSO = %s, WHERE ID_CURSO = %s and RFC=%s",id_curso, rfc)
+        conexion.conn.commit()
+        conexion.conn.close()
+        return affected
+
+    def obtener_aspirantescursos(self):
+        conexion = Database.Database()
+        students = []
+        with conexion.cursor as cursor:
+            cursor.execute("SELECT * FROM aspirantes_cursos")
+            students = cursor.fetchall()
+        conexion.conn.close()
+        return students
+    
+    def obtener_aspirantescursos_rfc(self,rfc):
+        conexion = conexion = Database.Database()
+        student = None
+        with conexion.cursor as cursor:
+            cursor.execute(
+                "SELECT * FROM aspirantes_cursos WHERE RFC = %s",rfc)
+            student = cursor.fetchone()
+        conexion.conn.close()
+        return student                
